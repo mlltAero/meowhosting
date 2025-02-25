3cc7ca66abd97db926b09787e40a2419cc5e91ef
mllt <59761330+mlltAero@users.noreply.github.com>
2025-02-25 10:28:19 +0200

    Fix typo in datetime import in logd function and versioncontrol

--- backend/meowLib/logd/logd.py
+++ backend/meowLib/logd/logd.py
@@ -5,6 +5,6 @@ just makes life easier by printing everything in a log format
 import datetime
 
 def logd(text_input):
-    now = datetime.datetim20e.now()
+    now = datetime.datetime.now()
     now = now.strftime("%Y-%m-%d %H:%M:%S")
     print(f'[{now}] {text_input}')
